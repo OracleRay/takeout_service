054f1b2e8463f823dba53fc605448327e33fee27
test <248107148@qq.com>
2023-05-03 09:58:19 +0800

    添加菜品管理启售、停售和批量删除功能

--- src/main/java/imau/ray/takeout_service/service/DishService.java
+++ src/main/java/imau/ray/takeout_service/service/DishService.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import imau.ray.takeout_service.dto.DishDto;
 import imau.ray.takeout_service.entity.Dish;
 
+import java.util.List;
+
 public interface DishService extends IService<Dish> {
     // 新增菜品，同时插入菜品对应的口味，需要操作dish、dish_flavor两张表
     public void saveWithFlavor(DishDto dishDto);
@@ -13,4 +15,7 @@ public interface DishService extends IService<Dish> {
 
     // 更新菜品信息和口味信息
     public void updateWithFlavor(DishDto dishDto);
+
+    //根据传过来的id批量或者是单个的删除菜品
+    void deleteByIds(List<Long> ids);
 }
